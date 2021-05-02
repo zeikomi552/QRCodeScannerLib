c27864ee5e00f71421588914d0aa562c42da3d27
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-05-02 11:11:18 +0900

    初版作成

---
+++ QRCodeScannerLib/ScannerControlParameters.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRCodeScannerLib
+{
+    public class ScannerControlParameters
+    {
+        /// <summary>
+        /// 読み取り待機
+        /// トリガスイッチが連続読み取りモード 1 または 2 に設定されている時に限り、「Z」、「READOFF」、「LOFF」コマンドが受信されるとスキャナは読み取り待機状態に入ります。
+        /// </summary>
+        public class WaitForRead
+        {
+            /// <summary>
+            /// 読み取り待機
+            /// </summary>
+            public const string Z = "Z";
+
+            /// <summary>
+            /// 読み取り待機
+            /// </summary>
+            public const string READOFF = "READOFF";
+
+            /// <summary>
+            /// 読み取り待機
+            /// </summary>
+            public const string LOFF = "LOFF";
+        }
+        /// <summary>
+        /// 読み取り可能トリガスイッチが連続読み取りモード 1 または 2 に設定されている時に限り、「R」、「READON」、「LON」コマンドが受信されると、スキャナは読み取り可能状態に入ります。
+        /// </summary>
+        public class ReadEnable
+        {
+            public const string R = "R";
+            public const string READON = "READON";
+            public const string LON = "LON";
+        }
+
+        /// <summary>
+        /// ブザー鳴動
+        /// コマンド受信後 100ms 以内に、決められた時間だけブザーを鳴動させます。
+        /// 「B1」: 約 60ms，約 80ms，約 120ms または約 140ms 間鳴動
+        /// 「B2」: 約 120ms 間鳴動
+        /// 「B3」: 約 240ms 間鳴動
+        /// ブザーの鳴動が禁止されている場合や、読み取り待機状態でも鳴動可能です。
+        /// </summary>
+        public class Buzzer
+        {
+            public const string B1 = "B1";
+            public const string B2 = "B2";
+            public const string B3 = "B3";
+        }
+
+        /// <summary>
+        /// ブザー鳴動(音色指定)
+        /// 上記 B1～B3 コマンドに、この音色オプションを追加すること
+        /// で、音色別にブザーを鳴動させます。
+        /// 「H」: 高音(4.0 kHz)で鳴動
+        /// 「M」: 中音(3.0 kHz で鳴動
+        /// 「L」: 低音(2.8 kHz)で鳴動
+        /// </summary>
+        public class BuzzerSound
+        {
+            public const string BH1 = "BH1"; public const string BM1 = "BM1"; public const string BL1 = "BL1";
+            public const string BH2 = "BH2"; public const string BM2 = "BM2"; public const string BL2 = "BL2";
+            public const string BH3 = "BH3"; public const string BM3 = "BM3"; public const string BL3 = "BL3";
+        }
+
+        /// <summary>
+        /// 青色表示 LED 点灯
+        /// コマンド受信後 100ms 以内に、約 500ms 間点灯します。
+        /// </summary>
+        public const string LB = "LB";
+
+        /// <summary>
+        /// 緑色表示 LED 点灯
+        /// コマンド受信後 100ms 以内に、約 500ms 間点灯します。
+        /// </summary>
+        public const string LG = "LG";
+        /// <summary>
+        /// 赤色表示 LED 点灯
+        /// コマンド受信後 100ms 以内に、約 500ms 間点灯します。
+        /// </summary>
+        public const string LR = "LR";
+
+        /// <summary>
+        /// トリガスイッチコントロール
+        /// </summary>
+        public class TriggerSwitchControl
+        {
+            /// <summary>
+            /// オートオフモード
+            /// </summary>
+            public const string U1 = "U1";
+            /// <summary>
+            /// モメンタリスイッチモード
+            /// </summary>
+            public const string U2 = "U2";
+            /// <summary>
+            /// オルタネートスイッチモード
+            /// </summary>
+            public const string U3 = "U3";
+            /// <summary>
+            /// 連続読み取りモード1
+            /// </summary>
+            public const string U4 = "U4";
+            /// <summary>
+            /// 連続読み取りモード2
+            /// </summary>
+            public const string U5 = "U5";
+            /// <summary>
+            /// オートセンスモード
+            /// </summary>
+            public const string U6 = "U6";
+            /// <summary>
+            /// オートスタンドモード
+            /// </summary>
+            public const string U7 = "U7";
+            /// <summary>
+            /// モメンタリスイッチモード（反転タイプ）
+            /// </summary>
+            public const string U8 = "U8";
+        }
+
+        /// <summary>
+        /// パラメータ記憶
+        /// U1～U8 コマンドで設定した値をフラッシュ ROM へ記憶します。
+        /// このコマンドを与えないと、U1～U8 コマンドで設定した値は電
+        /// 源を切断すると元に戻ります。
+        /// </summary>
+        public const string PW = "PW";
+
+        /// <summary>
+        /// ソフトウエアバージョンの要求
+        /// ＜スキャナ応答＞「Ver.n.nn」
+        /// n.nn：バージョン No. （例：Ver.1.00）
+        /// </summary>
+        public const string VER = "VER";
+
+        /// <summary>
+        /// 設定パラメータバージョンの要求
+        /// 設定ソフトとの接続時にスキャナの設定パラメータのバージョン No.を確認できます。
+        /// ＜スキャナ応答＞「Ver.n.nn.mm」
+        /// n.nn.mm：バージョン No. （例：Ver.1.00.00）
+        /// mm：設定パラメータバージョン No.
+        /// </summary>
+        public const string VERF = "VERF";
+        /// <summary>
+        /// スキャンエントリーモードの要求
+        /// n 点照合読み取り時、「マスター読み取り登録」状態に移行し、
+        /// マスターコードの読み取りを行うことにより登録をすること
+        /// ができます。登録したマスターコードデータはフラッシュ ROMへ記憶します。
+        /// </summary>
+        public const string E = "E";
+        /// <summary>
+        /// スキャナ ID (シリアルナンバー)の要求
+        /// ＜スキャナ応答＞「ID.XXXXXXXXXXnnnnnn」
+        /// XXXXXXXXXX：固有ナンバー nnnnnn：シリアルナンバー
+        /// </summary>
+        public const string ID = "ID";
+        /// <summary>
+        /// トリガスイッチ機能の許可
+        /// トリガスイッチコントロールを有効状態にします。
+        /// </summary>
+        public const string TMON = "TMON";
+        /// <summary>
+        /// トリガスイッチ機能の無効
+        /// トリガスイッチコントロールを無効状態にし、読み取り待機状
+        /// 態にします。
+        /// </summary>
+        public const string TMOFF = "TMOFF";
+        /// <summary>
+        /// 読み取り失敗
+        /// 連続読み取りモード 1 または 2 に設定されている場合、読み取
+        /// り可能状態で読み取りを完了せずに読み取り待機状態となっ
+        /// たとき送信します。転送の要否を選択できます。
+        /// </summary>
+        public const string ERROR = "ERROR";
+        /// <summary>
+        /// 照合一致
+        /// 照合一致時のデータ出力を「OK」にした場合に限り、照合モー
+        /// ドで読み取りし、マスターデータとデータ照合が一致したとき送信します。
+        /// </summary>
+        public const string OK = "OK";
+        /// <summary>
+        /// 照合不一致
+        /// 照合不一致時のデータ出力を「NG」にした場合に限り、照合モ
+        /// ードで読み取りし、マスターデータとデータ照合が一致しなか
+        /// ったとき送信します。
+        /// </summary>
+        public const string NG = "NG";
+
+
+    }
+}
---
+++ QRCodeScannerLib/ScannerManager.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QRCodeScannerLib.ScannerControlParameters;
+
+namespace QRCodeScannerLib
+{
+	public class ScannerDataRecieveEventArgs : EventArgs
+	{
+		public SerialDataReceivedEventArgs OrignalArgs { get; set; }
+		public string Message { get; set; }
+
+	}
+	public class ScannerManager
+	{
+		/// <summary>
+		/// キャリッジリターン
+		/// </summary>
+		public static char CR = Convert.ToChar(0x0d);
+		#region Connect状態かどうかを表すフラグ(true:接続中 false:未接続)[IsConnect]プロパティ
+		/// <summary>
+		/// Connect状態かどうかを表すフラグ(true:接続中 false:未接続)[IsConnect]プロパティ用変数
+		/// </summary>
+		bool _IsConnect = false;
+		/// <summary>
+		/// Connect状態かどうかを表すフラグ(true:接続中 false:未接続)[IsConnect]プロパティ
+		/// </summary>
+		public bool IsConnect
+		{
+			get
+			{
+				return _IsConnect;
+			}
+			set
+			{
+				if (!_IsConnect.Equals(value))
+				{
+					_IsConnect = value;
+				}
+			}
+		}
+		#endregion
+
+		#region COMポート[ComPort]プロパティ
+		/// <summary>
+		/// COMポート[ComPort]プロパティ用変数
+		/// </summary>
+		int _ComPort = 3;
+		/// <summary>
+		/// COMポート[ComPort]プロパティ
+		/// </summary>
+		public int ComPort
+		{
+			get
+			{
+				return _ComPort;
+			}
+			set
+			{
+				if (!_ComPort.Equals(value))
+				{
+					_ComPort = value;
+				}
+			}
+		}
+		#endregion
+
+		#region ポート名
+		/// <summary>
+		/// ポート名
+		/// </summary>
+		public string PortName
+		{
+			get
+			{
+				return "COM" + this._ComPort.ToString();
+			}
+		}
+		#endregion
+
+		#region エンコード[Encoding]プロパティ
+		/// <summary>
+		/// エンコード[Encoding]プロパティ用変数
+		/// </summary>
+		Encoding _Encoding = Encoding.GetEncoding("shift-jis");
+		/// <summary>
+		/// エンコード[Encoding]プロパティ
+		/// </summary>
+		public Encoding Encoding
+		{
+			get
+			{
+				return _Encoding;
+			}
+			set
+			{
+				if (_Encoding == null || !_Encoding.Equals(value))
+				{
+					_Encoding = value;
+				}
+			}
+		}
+		#endregion
+
+		#region シリアル通信用オブジェクト[SerialPort]プロパティ
+		/// <summary>
+		/// シリアル通信用オブジェクト[SerialPort]プロパティ用変数
+		/// </summary>
+		SerialPort _SerialPort = null;
+		/// <summary>
+		/// シリアル通信用オブジェクト[SerialPort]プロパティ
+		/// </summary>
+		public SerialPort SerialPort
+		{
+			get
+			{
+				return _SerialPort;
+			}
+			set
+			{
+				if (_SerialPort == null || !_SerialPort.Equals(value))
+				{
+					_SerialPort = value;
+				}
+			}
+		}
+		#endregion
+
+		#region データの受信処理
+		/// <summary>
+		/// データの受信処理
+		/// </summary>
+		public EventHandler DataReceived;
+		#endregion
+
+		#region コンストラクタ
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="com_port">COMポート</param>
+		/// <param name="enc">エンコード</param>
+		public ScannerManager(int com_port, Encoding enc)
+        {
+			this.ComPort = com_port;	// COMポートの設定
+			this.Encoding = enc;		// エンコードの設定
+        }
+		#endregion
+
+		#region AT(ハンディスキャナ)接続処理
+		/// <summary>
+		/// AT(ハンディスキャナ)接続処理
+		/// </summary>
+		public void Connect()
+		{
+			try
+			{
+				this._SerialPort = new SerialPort(this.PortName);
+
+				this._SerialPort.DataReceived -= _SerialPort_DataReceived;
+				this._SerialPort.DataReceived += _SerialPort_DataReceived;
+				this._SerialPort.Open();
+
+				this._SerialPort.DtrEnable = true;  // DTRの有効化
+				this._SerialPort.RtsEnable = true;  // RTSの有効化
+
+				this.IsConnect = true;  // 接続中に変更
+				System.Threading.Thread.Sleep(100); // 通信安定まで一瞬待つ
+			}
+			catch
+			{
+				this.SerialPort.Close();
+				this.SerialPort.Dispose();
+				this.SerialPort = null;
+				this.IsConnect = false;  // 未接続に変更
+				throw;
+			}
+		}
+		#endregion
+
+		#region AT(ハンディスキャナ)切断処理
+		/// <summary>
+		/// AT(ハンディスキャナ)切断処理
+		/// </summary>
+		public void Disconnect()
+		{
+			try
+			{
+				if (this.SerialPort != null)
+				{
+					this.SerialPort.Close();
+					this.SerialPort.Dispose();
+					this.SerialPort = null;
+					this.IsConnect = false;  // 未接続に変更
+				}
+			}
+			catch
+			{
+				throw;
+			}
+		}
+		#endregion
+
+		#region U1～U8のモードを指定します
+		/// <summary>
+		/// U1～U8のモードを指定します
+		/// </summary>
+		/// <param name="mode">U1～U8のScannerControlParameters.TriggerSwitchControl以下のモードを指定します</param>
+		/// <param name="wait">設定後の安定までの待ち時間(ミリ秒)</param>
+		public void ModeSet(string mode, int wait=100)
+		{
+			try
+            {
+				this._SerialPort.WriteLine(mode + ScannerManager.CR);
+				System.Threading.Thread.Sleep(wait);
+
+				this._SerialPort.WriteLine(ScannerControlParameters.WaitForRead.Z + ScannerManager.CR);
+				System.Threading.Thread.Sleep(wait);
+
+				this._SerialPort.WriteLine(ScannerControlParameters.ReadEnable.R + ScannerManager.CR);
+
+				// シリアルポート上に残っているバッファをクリア
+				BufferClear();
+			}
+			catch
+            {
+				this.SerialPort.Close();
+				this.SerialPort.Dispose();
+				this.SerialPort = null;
+				this.IsConnect = false;  // 未接続に変更
+				throw;
+            }
+		}
+		#endregion
+
+		#region バッファのクリア処理
+		/// <summary>
+		/// バッファのクリア処理
+		/// </summary>
+		public void BufferClear()
+		{
+			this.SerialPort.DiscardInBuffer();
+		}
+		#endregion
+
+
+		private static string TrimCarriageReturn(string msg)
+		{
+
+			if (msg.Length > 0 && msg.Substring(msg.Length - 1).Equals(ScannerManager.CR))
+			{
+				return msg.Substring(0, msg.Length - 1);
+			}
+			else
+			{
+				return msg;
+			}
+		}
+		#region イベント処理
+		/// <summary>
+		/// イベント処理
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void _SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+			if (this.DataReceived != null)
+			{
+				int index = 0;
+				bool bFind = false;
+				string msg = string.Empty;
+
+				while (true)
+				{
+					// シリアルポートの電圧が安定するのを少し待つ
+					System.Threading.Thread.Sleep(50);
+
+					// シリアルポートからのデータ吸出し
+					string tmp_msg = this.SerialPort.ReadExisting();
+					msg = msg + tmp_msg;    // 文字列の結合
+
+                    if (tmp_msg.Length > 0 && tmp_msg.Substring(tmp_msg.Length - 1).Equals(this.CR))
+                    {
+                        bFind = true;
+                    }
+                    else
+                    {
+						// キャリッジリターンを見つけた && 取得した文字列が空
+						if (bFind && string.IsNullOrEmpty(tmp_msg))
+						{
+							msg = GetRecieveData(msg);
+
+							// イベントの打ち上げ(TODO)
+							ScannerDataRecieveEventArgs args = new ScannerDataRecieveEventArgs();
+							args.OrignalArgs = e;
+
+							if (msg.Length > 0 && msg.Substring(msg.Length - 1).Equals(this.CR))
+							{
+								args.Message = msg.Substring(0, msg.Length - 1);
+							}
+							else
+                            {
+								args.Message = msg;
+							}
+
+							if (this.DataReceived != null)
+							{
+								this.DataReceived(sender, args);
+							}
+
+							break;
+						}
+						else
+						{
+							bFind = false;
+						}
+                    }
+
+					// リトライ回数が規定を超えた場合強制的に抜ける
+					if (index >= 10)
+					{
+						ScannerDataRecieveEventArgs args = new ScannerDataRecieveEventArgs();
+						args.OrignalArgs = e;
+						args.Message = msg;
+						if (this.DataReceived != null)
+						{
+							this.DataReceived(sender, args);
+						}
+
+						break;
+					}
+					index++;
+				}
+			}
+		}
+		#endregion
+
+		#region データの受信処理
+		/// <summary>
+		/// データの受信処理
+		/// </summary>
+		/// <param name="msg">受信データ</param>
+		/// <returns>連結したデータ</returns>
+		private string GetRecieveData(string msg)
+		{
+			int index = msg.IndexOf(this.CR);
+
+			if (index >= 0 && index + 1 < msg.Length)
+			{
+				string tmp_msg1 = msg.Substring(0, index + 1);
+				string tmp_msg2 = msg.Substring(index + 1);
+
+				if (tmp_msg2.Substring(0, 1).Equals("\n"))
+				{
+					return tmp_msg1 + GetRecieveData(tmp_msg2);
+				}
+				else
+				{
+					return tmp_msg1;
+				}
+			}
+			else
+			{
+				return msg;
+			}
+		}
+		#endregion
+	}
+}
